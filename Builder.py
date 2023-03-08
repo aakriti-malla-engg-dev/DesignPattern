00df2fbc99dfccebad55b8bf6aeb72246093fb69
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-03-08 16:51:56 +0530

    Builder.py file created

---
+++ Builder.py
@@ -0,0 +1,12 @@
+class Address:
+    def __init__(self, zipNo, street):
+        self.zipNo = zipNo
+        self.street = street
+
+
+class User:
+    def __init__(self, name, age, phone, address):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
