97d003e6eaeaa7b68f71392b1cc4cfd9a523c670
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-03-08 18:54:57 +0530

    traditional

--- Builder.py
+++ Builder.py
@@ -5,8 +5,29 @@ class Address:
 
 
 class User:
-    def __init__(self, name, age, phone, address):
+    def __init__(self, name):
         self.name = name
-        self.age = age
-        self.phone = phone
-        self.address = address
+
+
+class UserBuilder:
+    def __init__(self, name):
+        self.user = User(name)
+
+    def setAge(self, age):
+        self.user.age = age
+        return self
+
+    def setPhone(self, phone):
+        self.user.phone = phone
+        return self
+
+    def setAddress(self, address):
+        self.user.address = address
+        return self
+
+    def build(self):
+        return self.user
+
+
+builder = UserBuilder('Bob').setAge(30).build()
+print(builder.name, builder.age)
